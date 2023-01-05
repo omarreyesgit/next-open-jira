60c45c0f1d36ad51d4c5971003c06fb3f0a058f0
omar <arturreyes90@gmail.com>
2023-01-05 17:38:25 +0100

    Configurando Material UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+const basicTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={basicTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
